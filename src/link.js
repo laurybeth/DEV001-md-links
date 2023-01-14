5a85d596e8fd27c8049a831c566bd45c47a664ba
laurybeth <laurybeth@msn.com>
2023-01-14 01:02:27 -0500

    se obtiene un array de tokens con funcion parse de markdown-it

--- src/link.js
+++ src/link.js
@@ -5,8 +5,22 @@ const getLinks = (path) => {
   fs.readFile(path, { encoding: 'utf8' })
     .then((content) => {
       const md = new MarkdownIt();
-      const result = md.render(content);
-      console.log('result: ', result);
+      /* const toHtml = md.render(content);
+      console.log('toHtml', toHtml); */
+      const result = md.parse(content, {});
+
+      for (let i = 0; i < result.length; i++) {
+        if (result[i].type === 'inline') {
+          //console.log('inline: ', result[i]);
+          console.log('children: ', result[i].children);
+          for (let j = 0; j < result[i].children.length; j++) {
+            console.log('children[j]: ', result[i].children[j]);
+            if (result[i].children[j].type === 'link_open') {
+              console.log('attrs: ', result[i].children[j].attrs);
+            }
+          }
+        }
+      }
     })
     .catch();
 };
