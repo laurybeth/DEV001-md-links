ee454cf1d17db6972300eabd6000332989cf031b
laurybeth <laurybeth@msn.com>
2023-01-28 17:51:42 -0500

    configuracioón previa para el script ejecutable

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const validateCommands = (commands) => {
+const validateCommands = (commands) => new Promise((resolve, reject) => {
   const options = {};
 
   if (commands.includes('--validate')) {
@@ -9,8 +9,21 @@ const validateCommands = (commands) => {
     options.stats = true;
   }
 
+  resolve(options);
+});
+
+/* const validateCommands = (commands) => {
+
+  if (commands.includes('--validate')) {
+    options.validate = true;
+  }
+
+  if (commands.includes('--stats')) {
+    options.stats = true;
+  }
+
   return options;
-};
+}; */
 
 module.exports = {
   validateCommands,
